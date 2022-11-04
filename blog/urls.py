9694d731182e505c1345ee2f31e5f0a1ef6f2bb3
tnakai <tnakai@protonmail.com>
2022-11-04 23:18:39 +0900

    Added view and template for detailed blog post as well as CSS fot the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    # post/ はURLが post に続けて / で始まることを意味
+    # <int:pk> はDjangoは整数の値を期待し、その値がpkという名前の変数でビューに渡されることを意味
+    # URLの最後に再び / が必要
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from django.utils import timezone
 # .はカレントディレクトリ(拡張子は不要)
 from .models import Post
 
+from django.shortcuts import render, get_object_or_404
+
 # Create your views here.
 
 # 投稿の一覧を表示
@@ -11,3 +13,7 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     # {} の中に引数を記述する時は、名前と値をセット('posts'が名前で、後ろの方の posts は値、クエリセット)
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
