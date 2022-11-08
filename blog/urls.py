47ae1ba93d0bcf33211eafa63d3923f62d928c6f
tnakai <tnakai@protonmail.com>
2022-11-08 23:18:11 +0900

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     # <int:pk> はDjangoは整数の値を期待し、その値がpkという名前の変数でビューに渡されることを意味
     # URLの最後に再び / が必要
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
