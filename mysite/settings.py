08b9c9f34cf15868cff7061dad1fa35724bffee0
tnakai <tnakai@protonmail.com>
2022-11-08 23:33:37 +0900

    change settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,8 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # ↓追加部分
-STATIC_ROOT = BASE_DIR / 'static'
-
+# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
